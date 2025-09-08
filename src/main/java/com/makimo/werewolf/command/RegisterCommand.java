e67192ead1619604c46df5f758731ef57940a3a4
Rithys <rithys.wisdom@gmail.com>
2025-09-08 14:30:57 +0900

    レジストリ関連の追加

---
+++ src/main/java/com/makimo/werewolf/command/RegisterCommand.java
@@ -0,0 +1,33 @@
+package com.makimo.werewolf.command;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import static com.makimo.werewolf.Werewolf.MOD_ID;
+
+@Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class RegisterCommand {
+    @SubscribeEvent
+    public static void onRegisterWWCommand(RegisterCommandsEvent event) {
+        LiteralArgumentBuilder<CommandSourceStack> builder = Commands.literal("ww")
+            .then(Commands.literal("start")
+                .executes(context -> {
+                    //ここに処理を書く
+                    context.getSource().getPlayerOrException().sendSystemMessage(Component.nullToEmpty("[Dev]:Success!"));
+                    return Command.SINGLE_SUCCESS;
+                }))
+            .then(Commands.literal("setting")
+                .executes(context -> {
+                    //ここに処理を書く
+                    context.getSource().getPlayerOrException().sendSystemMessage(Component.nullToEmpty("[Dev]:Success!"));
+                    return Command.SINGLE_SUCCESS;
+                }));
+        event.getDispatcher().register(builder);
+    }
+}
