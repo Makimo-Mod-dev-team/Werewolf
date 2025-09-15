c9c044cef94653b35d9f8e2475fe763dd20397c4
Rithys <rithys.wisdom@gmail.com>
2025-09-15 23:31:15 +0900

    諸々の変更

--- src/main/java/com/makimo/werewolf/item/SmallLightItem.java
+++ src/main/java/com/makimo/werewolf/item/SmallLightItem.java
@@ -3,17 +3,12 @@ package com.makimo.werewolf.item;
 import com.makimo.werewolf.util.DetectPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 import virtuoel.pehkui.api.ScaleData;
 import virtuoel.pehkui.api.ScaleTypes;
-
-import java.util.List;
 
 public class SmallLightItem extends Item {
     public SmallLightItem(Properties properties) {
@@ -24,6 +19,11 @@ public class SmallLightItem extends Item {
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         if (level.isClientSide) {
             return InteractionResultHolder.pass(player.getItemInHand(hand));
+        }
+        Player targetPlayer = DetectPlayer.DetectPlayerFromLayCast(player, 3);
+        if (targetPlayer != null) {
+            ScaleData data = ScaleTypes.BASE.getScaleData(targetPlayer);
+            data.setTargetScale(0.5f);
         }
         return InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), level.isClientSide());
     }
